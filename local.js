705b85575a5518cdeba9b1a0c708ec35ce59c2dc
sagar kumar rana <s1g1rk1m1r@gmail.com>
2023-02-06 14:57:10 +0530

    testing...

---
+++ local.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+app.use("/static", express.static(path.join(__dirname, "uploads")));
+const io = require("socket.io").listen(server);
+
+// static files
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+// app.get("/", (req, res) => {
+//   res.send("hello from lootlearn webrtc server");
+// });
+const listner = server.listen(process.env.PORT, function () {
+  console.log("Listening on", listner.address().port);
+});
+
+require("./socket")(io, "./../src/recordings/");
